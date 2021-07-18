ba3b785a024fe32664e4aaa9494c77e024ef2e65
Nigecat <nigel@nigelq.com>
2021-07-19 09:33:54 +1000

    Configure dslib ffi interface

---
+++ dslib/src/lib.rs
@@ -0,0 +1,49 @@
+#[cfg(windows)]
+mod windows;
+#[cfg(windows)]
+use windows::scan as _scan;
+
+#[cfg(unix)]
+mod unix;
+#[cfg(unix)]
+use unix::scan as _scan;
+
+#[macro_use]
+extern crate napi_derive;
+
+use napi::{CallContext, JsObject, JsString, JsUnknown, Result};
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::path::PathBuf;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum File {
+    File { path: PathBuf, size: usize },
+    Directory(HashMap<String, File>),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ScanResult {
+    base: PathBuf,
+    files: File,
+}
+
+#[js_function(1)]
+fn scan(ctx: CallContext) -> Result<JsUnknown> {
+    let dir: PathBuf = ctx.get::<JsString>(0)?.into_utf8()?.as_str()?.into();
+    let res = _scan(dir.clone());
+
+    match res {
+        Ok(f) => ctx.env.to_js_value(&ScanResult {
+            base: dir,
+            files: f,
+        }),
+        Err(e) => Err(napi::Error::new(napi::Status::Unknown, e.to_string())),
+    }
+}
+
+#[module_exports]
+fn init(mut exports: JsObject) -> Result<()> {
+    exports.create_named_method("scan", scan)?;
+    Ok(())
+}
---
+++ dslib/src/windows/mod.rs
@@ -0,0 +1,6 @@
+use super::File;
+use std::path::PathBuf;
+
+pub fn scan(dir: PathBuf) -> Result<File, Box<dyn ::std::error::Error>> {
+    todo!();
+}
