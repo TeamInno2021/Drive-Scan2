f9c82b78c9733e63a7fb17b1ac1fbf6b8b133825
Nigecat <nigel@nigelq.com>
2021-07-22 18:31:01 +1000

    Implement mft record parser

--- dslib/src/windows/mft/mod.rs
+++ dslib/src/windows/mft/mod.rs
@@ -3,21 +3,29 @@ mod raw;
 use super::drive::DriveInfo;
 use super::winapi::get_last_error;
 use super::OsError;
-use raw::MftRecord;
+use raw::*;
 
+use num_traits::FromPrimitive;
 use std::ffi::c_void;
 use std::ptr;
 
 use winapi::um::fileapi::ReadFile;
 use winapi::um::minwinbase::{OVERLAPPED_u, OVERLAPPED};
 
+/// Special identifier for the root directory
+const ROOT: u32 = 5;
+
+#[derive(Debug, Default)]
 pub struct MftNode {
-    // todo
+    pub attributes: MftNodeAttributes,
+    pub parent_node_index: Option<u32>,
+    pub size: Option<u64>,
+    pub name_index: Option<i32>,
 }
 
 pub fn process(drive: DriveInfo) -> Result<Vec<MftNode>, OsError> {
     // Read the $MFT record, this is always the first entry in the MFT table and acts as a record of the files in the volume
-    let mut mft: Vec<u8> = Vec::with_capacity(64 * 1024); // 64kb
+    let mut mft: Vec<u8> = Vec::with_capacity(256 * 1024); // 256kb
 
     unsafe {
         let mut size = 0;
@@ -44,16 +52,175 @@ pub fn process(drive: DriveInfo) -> Result<Vec<MftNode>, OsError> {
         }
 
         if drive.bytes_per_mft_record != size as u64 {
-            // todo make proper error
-            panic!("Could not read complete volume data");
+            return Err("could not read complete volume data".into());
         }
 
         mft.set_len(size as usize);
     }
 
-    // Parse the $MFT record
-    let mft = unsafe { (mft.as_mut_ptr() as *mut MftRecord).as_ref().unwrap() };
-    println!("{:#?}", mft);
+    // Process the $MFT record
+    let mft_record = unsafe { (mft.as_mut_ptr() as *mut MftRecord).as_ref().unwrap() };
+    info!("Read $MFT record: {:#?}", mft_record);
+
+    let node = process_record(&drive, &mut mft, &mft_record)?;
+    info!("{:#?}", node);
 
     Ok(Vec::default())
+}
+
+fn process_record(
+    drive: &DriveInfo,
+    buffer: &mut [u8],
+    record: &MftRecord,
+) -> Result<MftNode, OsError> {
+    // Sanity checks
+    {
+        let base = ((record.base_file_record.inode_number_high as u64) << 32)
+            + record.base_file_record.inode_number_low as u64; // note that this should always be 0
+
+        // Ensure this is an active file record
+        if base != 0 || record.header.ty != raw::FILE || record.flags & 1 != 1 {
+            return Err("unable to interpret mft record".into());
+        }
+
+        if record.attribute_offset as u64 >= drive.bytes_per_mft_record {
+            return Err("record attribute outside expected range, the mft may be corrupt".into());
+        }
+        if record.bytes_in_use as u64 > drive.bytes_per_mft_record {
+            return Err("record large than buffer, the mft may be corrupt".into());
+        }
+    }
+
+    // ---------- Process node
+
+    let mut node = MftNode::default();
+
+    // Default this entry to the root
+    node.parent_node_index = Some(ROOT);
+
+    if record.flags & 2 == 2 {
+        node.attributes |= MftNodeAttributes::DIRECTORY;
+    }
+
+    // ----- Process attributes
+
+    let buf_length = drive.bytes_per_mft_record - record.attribute_offset as u64;
+
+    let mut offset = 0;
+    while offset < buf_length {
+        // Parse attribute
+        let attribute = unsafe {
+            (buffer
+                .as_mut_ptr()
+                .wrapping_add(record.attribute_offset as usize) // fixme [IMPORTANT]: remember to add this to `buffer` when using as `ptr` translation vector
+                .wrapping_add(offset as usize) as *mut MftNodeAttribute)
+                .as_mut()
+                .unwrap()
+        };
+
+        // trace!("Attribute {} + {}", offset, unsafe { *&attribute.length });
+
+        // Exit loop at end marker
+        if (offset + 4 <= buf_length)
+            && *unsafe {
+                (attribute as *mut MftNodeAttribute as *mut u32)
+                    .as_ref()
+                    .unwrap()
+            } == 0xFFFFFFFF
+        {
+            break;
+        }
+
+        // Sanity check
+        if (offset + 4 > buf_length)
+            || attribute.length < 3
+            || (offset + attribute.length as u64 > buf_length)
+        {
+            return Err("attribute large than data, the mft may be corrupt".into());
+        }
+
+        // Attribute list must be processed last
+        if attribute.attribute_type == MftNodeAttributeType::AttributeList as u32 {
+            continue;
+        }
+
+        // Parse extended attribute info
+        if attribute.non_resident == 0 {
+            let resident_attribute = unsafe {
+                (attribute as *mut MftNodeAttribute as *mut MftNodeResidentAttribute)
+                    .as_ref()
+                    .unwrap()
+            };
+
+            match FromPrimitive::from_u32(attribute.attribute_type) {
+                Some(MftNodeAttributeType::FileName) => {
+                    let attribute_filename = unsafe {
+                        (buffer
+                            .as_mut_ptr()
+                            .wrapping_add(record.attribute_offset as usize)
+                            .wrapping_add(offset as usize)
+                            .wrapping_add(resident_attribute.value_offset as usize)
+                            as *mut MftNodeAttributeFileName)
+                            .as_ref()
+                            .unwrap()
+                    };
+
+                    // todo remove this restriction
+                    if attribute_filename.parent_directory.inode_number_high > 0 {
+                        return Err("unsupported mft inode format (48 bit)".into());
+                    }
+
+                    node.parent_node_index =
+                        Some(attribute_filename.parent_directory.inode_number_low);
+
+                    if attribute_filename.name_type == 1 || node.name_index == Some(0) {
+                        println!("{}", unsafe { *&attribute_filename.name });
+                        // L: 1007
+                        unimplemented!(); // todo
+                    }
+                }
+                Some(MftNodeAttributeType::StandardInformation) => {
+                    let attribute_standardinformation = unsafe {
+                        (buffer
+                            .as_mut_ptr()
+                            .wrapping_add(record.attribute_offset as usize)
+                            .wrapping_add(offset as usize)
+                            .wrapping_add(resident_attribute.value_offset as usize)
+                            as *mut MftNodeAttributeStandardInformation)
+                            .as_ref()
+                            .unwrap()
+                    };
+
+                    // fixme this does not work either
+                    // println!("{}", unsafe {
+                    //     *&attribute_standardinformation.file_attributes as u64
+                    // });
+                    // let num = unsafe { *&attribute_standardinformation.file_attributes as usize };
+                    // // println!("{:?}", unsafe {
+                    //     (num as *const usize as *const MftNodeAttributes)
+                    //         .as_ref()
+                    //         .unwrap()
+                    // });
+                    // todo
+                    // fixme causes segfault when trying to print node object
+                    // node.attributes |= *unsafe {
+                    //     (attribute_standardinformation.file_attributes as *const usize
+                    //         as *const MftNodeAttributes)
+                    //         .as_ref()
+                    //         .unwrap()
+                    // };
+                }
+                Some(MftNodeAttributeType::Data) => {
+                    node.size = Some(resident_attribute.value_length as u64);
+                }
+                _ => (),
+            }
+        } else {
+            // todo
+        }
+
+        offset += attribute.length as u64;
+    }
+
+    Ok(node)
 }
--- dslib/src/windows/mft/raw.rs
+++ dslib/src/windows/mft/raw.rs
@@ -1,9 +1,17 @@
+use bitflags::bitflags;
+use num_derive::FromPrimitive;
+use num_traits::FromPrimitive;
+use std::convert::TryFrom;
+
+// ---------- MFT ----------
+
 pub const FILE: u32 = 0x454c4946;
 
 #[derive(Debug, Clone, Copy)]
 #[repr(packed(1))]
 pub struct MftRecordHeader {
-    pub ty: u32, // 0x454c4946 = 'FILE'
+    /// 0x454c4946 = 'FILE'
+    pub ty: u32, // todo convert to enum type (must be able to resize enum)
     pub usa_offset: u16,
     pub usa_count: u16,
     pub lsn: u64,
@@ -32,4 +40,175 @@ pub struct MftRecord {
     _padding: u16,
     pub mft_record_number: u32,
     pub update_seq_num: u16,
+}
+
+// ---------- MFT Attribute ----------
+
+bitflags! {
+    #[derive(Default,)]
+    pub struct MftNodeAttributes: usize {
+        const NULL = 0;
+
+        const READ_ONLY = 1 << 0;
+        const HIDDEN = 1 << 1;
+        const SYSTEM = 1 << 2;
+        const DIRECTORY = 1 << 3;
+        const ARCHIVE = 1 << 4;
+        const DEVICE = 1 << 5;
+        const NORMAL = 1 << 6;
+        const TEMPORARY = 1 << 7;
+        const SPARSE_FILE = 1 << 8;
+        const REPARSE_POINT = 1 << 9;
+        const COMPRESSED = 1 << 10;
+        const OFFLINE =  1 << 11;
+        const NOT_CONTENT_INDEXED = 1 << 12;
+        const ENCRYPTED = 1 << 13;
+    }
+}
+
+// impl FromPrimitive for MftNodeAttributes {
+//     fn from_u64(n: u64) -> Option<Self> {
+//         if n == 0 {
+//             Some(MftNodeAttributes::NULL)
+//         } else if n == 1 << 0 {
+//             Some(MftNodeAttributes::READ_ONLY)
+//         } else if n == 1 << 1 {
+//             Some(MftNodeAttributes::HIDDEN)
+//         } else if n == 1 << 2 {
+//             Some(MftNodeAttributes::SYSTEM)
+//         } else if n == 1 << 3 {
+//             Some(MftNodeAttributes::DIRECTORY)
+//         } else if n == 1 << 4 {
+//             Some(MftNodeAttributes::ARCHIVE)
+//         } else if n == 1 << 5 {
+//             Some(MftNodeAttributes::DEVICE)
+//         } else if n == 1 << 6 {
+//             Some(MftNodeAttributes::NORMAL)
+//         } else if n == 1 << 7 {
+//             Some(MftNodeAttributes::TEMPORARY)
+//         } else if n == 1 << 8 {
+//             Some(MftNodeAttributes::SPARSE_FILE)
+//         } else if n == 1 << 9 {
+//             Some(MftNodeAttributes::REPARSE_POINT)
+//         } else if n == 1 << 10 {
+//             Some(MftNodeAttributes::COMPRESSED)
+//         } else if n == 1 << 11 {
+//             Some(MftNodeAttributes::OFFLINE)
+//         } else if n == 1 << 12 {
+//             Some(MftNodeAttributes::NOT_CONTENT_INDEXED)
+//         } else if n == 1 << 13 {
+//             Some(MftNodeAttributes::ENCRYPTED)
+//         } else {
+//             None
+//         }
+//     }
+
+//     /// Note that this method will always return `None`, use an unsigned function instead
+//     fn from_i64(n: i64) -> Option<Self> {
+//         MftNodeAttributes::from_u64(u64::try_from(n).ok()?)
+//     }
+// }
+
+#[derive(Debug, Clone, Copy, FromPrimitive)]
+pub enum MftNodeAttributeType {
+    Invalid = 0x00,
+    StandardInformation = 0x10,
+    AttributeList = 0x20,
+    FileName = 0x30,
+    ObjectId = 0x40,
+    SecurityDescriptor = 0x50,
+    VolumeName = 0x60,
+    VolumeInformation = 0x70,
+    Data = 0x80,
+    IndexRoot = 0x90,
+    IndexAllocation = 0xA0,
+    Bitmap = 0xB0,
+    ReparsePoint = 0xC0,
+    EAInformation = 0xD0,
+    EA = 0xE0,
+    PropertySet = 0xF0,
+    LoggedUtilityStream = 0x100,
+}
+
+#[derive(Debug, Clone, Copy)]
+#[repr(packed(1))]
+pub struct MftNodeAttribute {
+    /// `MftNodeAttributeType`
+    pub attribute_type: u32, // todo convert to enum type (must be able to resize enum)
+    pub length: u32,
+    pub non_resident: u8,
+    pub name_length: u8,
+    pub name_offset: u16,
+    /// 0x0001 = Compressed, 0x4000 = Encrypted, 0x8000 = Sparse
+    pub flags: u16,
+    pub attribute_number: u16,
+}
+
+// ----- MFT Resident Attribute -----
+
+#[derive(Debug, Clone, Copy)]
+#[repr(packed(1))]
+pub struct MftNodeResidentAttribute {
+    pub attribute: MftNodeAttribute,
+    pub value_length: u32,
+    pub value_offset: u16,
+    /// 0x0001 = Indexed
+    pub flags: u16,
+}
+
+// ----- MFT Non-Resident Attribute -----
+
+#[derive(Debug, Clone, Copy)]
+#[repr(packed(1))]
+pub struct MftNodeNonResidentAttribute {
+    pub attribute: MftNodeAttribute,
+    pub starting_vsc: u64,
+    pub last_vsc: u64,
+    pub run_array_offset: u16,
+    pub compression_unit: u8,
+    _padding: [u8; 5],
+    pub allocated_size: u64,
+    pub data_size: u64,
+    pub initialized_size: u64,
+    pub compressed_size: u64, // only when compressed
+}
+
+// ----- MFT MftNodeAttributeType::FileName data -----
+
+#[derive(Debug, Clone, Copy)]
+#[repr(packed(1))]
+pub struct MftNodeAttributeFileName {
+    pub parent_directory: INodeMeta,
+    pub creation_time: u64,
+    pub change_time: u64,
+    pub last_write_time: u64,
+    pub last_access_time: u64,
+    pub allocated_size: u64,
+    pub data_size: u64,
+    pub file_attributes: u32,
+    _padding: u32,
+    pub name_length: u8,
+    /// NTFS = 0x01, DOS = 0x02
+    pub name_type: u8,
+    pub name: char,
+}
+
+// ----- MFT MftNodeAttributeType::StandardInformation data -----
+
+#[derive(Debug, Clone, Copy)]
+#[repr(packed(1))]
+pub struct MftNodeAttributeStandardInformation {
+    pub creation_time: u64,
+    pub file_change_time: u64,
+    pub mft_change_time: u64,
+    pub last_access_time: u64,
+    /// READ_ONLY = 0x01, HIDDEN = 0x02, SYSTEM = 0x04, VOLUME_ID = 0x08, ARCHIVE = 0x20, DEVICE = 0x40
+    pub file_attributes: u32,
+    pub maximum_versions: u32,
+    pub version_number: u32,
+    pub class_id: u32,
+    pub ownder_id: u32,
+    pub security_id: u32,
+    pub quota_charge: u64,
+    pub usn: u64,
 }
