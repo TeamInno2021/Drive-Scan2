bc0fc50575265cf2af87dd087e23c907a98dfc15
Nigecat <nigel@nigelq.com>
2021-07-28 16:23:09 +1000

    Split ffi functions into two

--- dslib/pkg/index.js
+++ dslib/pkg/index.js
@@ -1,13 +1,36 @@
 const dslib = require("./dslib.node");
 
 /** Dummy scan implementation while the real implementation is being developed */
-function scan() {
-    return JSON.parse(
-        '{"base":"C:\\\\","files":{"files":{"system32":{"files":{"wow, why is system32 empty thats weird":{"path":"C:\\\\system32\\\\wow, why is system32 empty thats weird","size":0}},"path":"C:\\\\system32","size":1},"dslib":{"files":{"src":{"files":{"audio.mp3":{"path":"C:\\\\dslib\\\\src\\\\audio.mp3","size":97612073},"video.mp4":{"path":"C:\\\\dslib\\\\src\\\\video.mp4","size":439598709}},"path":"C:\\\\dslib\\\\src","size":537210782},"package-lock.json":{"path":"C:\\\\dslib\\\\package-lock.json","size":48784},"build.rs":{"path":"C:\\\\dslib\\\\build.rs","size":42},"move.js":{"path":"C:\\\\dslib\\\\move.js","size":320}},"path":"C:\\\\dslib","size":537259928}},"path":"C:\\\\","size":537259929}}'
-    );
+async function scan(dir) {}
+
+function query(dir) {
+    return {
+        path: "",
+        size: 23,
+        files: [
+            {
+                path: "a_large_video.mp4",
+                size: 23,
+                directory: false,
+            },
+            {
+                path: "",
+                size: 64,
+                directory: false,
+            },
+            {
+                path: "",
+                size: 23,
+                directory: false,
+            },
+        ],
+    };
 }
 
 module.exports = {
+    init: dslib.init,
     scan,
-    _scan: dslib.scan,
+    query,
+    // scan: dslib.scan,
+    // query: dslib.query,
 };
--- dslib/pkg/test.ts
+++
@@ -1,3 +0,0 @@
-import { _scan } from "dslib";
-
-console.log(_scan("C:\\"));
