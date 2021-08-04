b15556c6a8abf45193f78117a898a7dbf7adf1e1
Slymiii <thomasjhart5@gmail.com>
2021-08-05 09:21:52 +1000

    Change page state for tree view

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
 import React, { Component } from "react";
 import * as Utility from "./utility";
+import * as Scan from "./scan";
+import * as Electron from "electron";
 
-export class App extends Component {      
+export class App extends Component<{}, { currentpage: string }> { 
+    
+    constructor(props:{}) {
+        super(props)
+        this.state = {currentpage:"directorypage"}
+    }
+
+    getDirectory() {
+        let result = Electron.remote.dialog.showOpenDialogSync({
+            properties: ["openDirectory"],
+        });
+        Scan.scan(result[0]);
+        console.log(Scan.query(result[0]));
+        this.setState({currentpage:"treeviewpage"})
+    }
+    
+
     render(): JSX.Element {
-        return <h3>
+        if(this.state.currentpage == "directorypage") {
+            return <h3>
             Select
-            <input id="button" type="button" value="Directory" onClick={Utility.getDirectory}/>
+            <input id="button" type="button" value="Directory" onClick={this.getDirectory.bind(this)}/>
             </h3>;
+        }
+        else {
+            return <h1>
+                something
+            </h1>
+        }
     }
 }
