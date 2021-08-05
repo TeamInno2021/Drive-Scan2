ce8c066f2b76ab470e93147f69f83cb65bdee836
Slymiii <58405605+Slymiii@users.noreply.github.com>
2021-08-06 09:18:58 +1000

    Tree View within splitter pane

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,11 @@ import * as Scan from "./scan";
 import * as Electron from "electron";
 import SplitterLayout from 'react-splitter-layout';
 import 'react-splitter-layout/lib/index.css';
+import TreeView from '@material-ui/lab/TreeView';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import TreeItem from '@material-ui/lab/TreeItem';
+import Path from 'path';
 
 export class App extends Component<{}, { currentpage: string }> { 
     
@@ -30,11 +35,48 @@ export class App extends Component<{}, { currentpage: string }> {
             </h3>;
         }
         else if(this.state.currentpage == "mainviewpage") {
+
+            let scandata = Scan.query("C:\\")
+                interface RenderTree {
+                id: string;
+                name: string;
+                children?: RenderTree[]; 
+                }
+
+                    let children = [];
+
+                    for (let i  = 0; i < scandata.files.length; i++) {
+                        console.log(i);
+                        children.push( {
+                        id: `${i}`,
+                        name: Path.basename(scandata.files[i].path) + " - " + scandata.files[i].size,
+                    },);
+                }
+
+                const data: RenderTree = {
+                    id: 'root',
+                    name: scandata.path + " - " + scandata.size,
+                    children:children
+                };
+
+                const renderTree = (nodes: RenderTree) => (
+                    <TreeItem key={nodes.id} nodeId={nodes.id} label={nodes.name}>
+                        {Array.isArray(nodes.children) ? nodes.children.map((node) => renderTree(node)) : null}
+                    </TreeItem>
+                );
             return <h1>
                 <SplitterLayout vertical={false}>
                     {/* TreeView */}
                     <div>
-                        Tom Put the TreeView Here
+
+                    <TreeView
+                        defaultCollapseIcon={<ExpandMoreIcon />}
+                        defaultExpanded={['root']}
+                        defaultExpandIcon={<ChevronRightIcon />}
+                    >
+                        {renderTree(data)}
+                    </TreeView>
+               
                     </div>
                     {/* Folder and Pie Views In Horizontal Splitter */}
                     <div>
