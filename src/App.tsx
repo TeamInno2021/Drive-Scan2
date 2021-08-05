9ca85d5540b334d1dfc30cd8ece3dc25221e88ae
Nigecat <nigel@nigelq.com>
2021-08-05 16:41:38 +1000

    Fix merge conflict

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import * as Utility from "./utility";
 
 export class App extends Component {      
     render(): JSX.Element {
-        console.log(Utility.strConvert(650000000));
         return <h3>
             Select
             <input id="button" type="button" value="Directory" onClick={Utility.getDirectory}/>
--- src/utility.ts
+++ src/utility.ts
@@ -1,50 +1,7 @@
-import * as Electron from 'electron'
-import * as Scan from "./scan"
+import * as Scan from "./scan";
+import * as Electron from "electron";
 
 export function getDirectory() {
     let result = Electron.remote.dialog.showOpenDialogSync({properties: ["openDirectory"]})
     Scan.scan(result[0]).then(console.log)
-}
-
-export function convertData() {
-    Scan.scan("result").then((data) => {
-        console.log(data);
-    });
-}
-
-export function strConvert(size:number) {
-    if (size < 1024) {
-        size = Math.ceil(size);
-        return `${size} bytes`;
-    }
-
-    else if (size < 1024**2) {
-        size = size/1024;
-        size = Math.ceil(size);
-        return `${size} kilobytes`;
-    }
-
-    else if (size < 1024**3) {
-        size = size/1024;
-        size = size/1024;
-        size = Math.ceil(size);
-        return `${size} megabytes`;
-    }
-
-    else if (size < 1024**4) {
-        size = size/1024;
-        size = size/1024;
-        size = size/1024;
-        size = Math.ceil(size);
-        return `${size} gigabytes`;
-    }
-
-    else if (size < 1024**5) {
-        size = size/1024;
-        size = size/1024;
-        size = size/1024;
-        size = size/1024;
-        size = Math.ceil(size);
-        return `${size} terabytes`;
-    }
 }
