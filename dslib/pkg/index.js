954c25b62c7f56fe8947eda8759d1830aa061883
Nigecat <nigel@nigelq.com>
2021-08-13 20:26:54 +1000

    Allow artifacts for multiple platforms

--- dslib/pkg/index.js
+++ dslib/pkg/index.js
@@ -1,4 +1,21 @@
-const dslib = require("./dslib.node");
+// Due to the way the webpack loader bundles native node modules,
+// the path must be hardcoded to allow the renaming of the target file
+let dslib;
+switch (process.platform) {
+    case "win32": {
+        dslib = require("./dslib-win32.node");
+        break;
+    }
+
+    // case "linux": {
+    //     dslib = require("./dslib-linux.node");
+    //     break;
+    // }
+
+    default: {
+        throw new Error("unsupported platform");
+    }
+}
 
 module.exports = {
     init: dslib.init,
