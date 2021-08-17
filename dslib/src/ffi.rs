abfe0eb66cecc29d4e5ed0776e58402ae9179f03
Nigecat <48661288+Nigecat@users.noreply.github.com>
2021-08-17 10:58:35 +1000

    Tree view (#13)

--- dslib/src/ffi.rs
+++ dslib/src/ffi.rs
@@ -8,21 +8,24 @@ pub fn init(ctx: CallContext) -> Result<JsUndefined> {
 }
 
 #[js_function(1)]
-pub fn scan(ctx: CallContext) -> Result<JsUnknown> {
-    let dir: PathBuf = ctx.get::<JsString>(0)?.into_utf16()?.as_str()?.into();
+pub fn scan(ctx: CallContext) -> Result<JsUndefined> {
+    let dir: PathBuf = ctx.get::<JsString>(0)?.into_utf8()?.as_str()?.into();
 
     match super::scan(dir) {
-        Ok(f) => ctx.env.to_js_value(&f),
+        Ok(_) => ctx.env.get_undefined(),
         Err(e) => Err(Error::new(Status::Unknown, e.to_string())),
     }
 }
 
 #[js_function(1)]
 pub fn query(ctx: CallContext) -> Result<JsUnknown> {
-    let dir: PathBuf = ctx.get::<JsString>(0)?.into_utf16()?.as_str()?.into();
+    let dir: PathBuf = ctx.get::<JsString>(0)?.into_utf8()?.as_str()?.into();
 
     match super::query(dir) {
-        Ok(dir) => ctx.env.to_js_value(&dir),
+        Ok(dir) => match dir {
+            Some(dir) => ctx.env.to_js_value(&dir),
+            None => Ok(ctx.env.get_undefined()?.into_unknown()),
+        },
         Err(e) => Err(Error::new(Status::Unknown, e.to_string())),
     }
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,66 @@
 import React, { Component } from "react";
-import * as Utility from "./utility";
 import * as Scan from "./scan";
 import * as Electron from "electron";
-import dslib, { File, query } from "dslib";
 
 //Splitter Layout
 import SplitterLayout from 'react-splitter-layout';
 import 'react-splitter-layout/lib/index.css';
-import { Button, ThemeProvider, Typography } from '@material-ui/core'
-import { createTheme } from "@material-ui/core/styles";
+import TreeView from '@material-ui/lab/TreeView';
+import dslib from "dslib";
+import path from "path";
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import TreeItem from '@material-ui/lab/TreeItem';
+import { strConvert } from "./conversion";
+import { Button, ThemeProvider, Typography } from '@material-ui/core';
+import { createTheme } from "@material-ui/core/styles";;
 
-export class App extends Component<{}, { currentPage: string, currentFolder: dslib.File, }> { 
-    
+export class App extends Component<{}, { currentPage: string, currentFolder: dslib.File, folderTree: dslib.File, rootPath: string }> { 
     constructor(props:{}) {
         super(props)
         this.state = {
-            currentPage: "scanpage",
-            currentFolder: { path: "", size: 0, children: [] }
+            currentPage:"scanpage", 
+            currentFolder: { 
+                path: "",
+                size: 0, 
+                children: [], 
+            },
+            folderTree: { 
+                path: "",
+                size: 0, 
+                children: [],
+            }, 
+            rootPath: "", 
         }
     }
 
-    getDirectory() {
+    async getDirectory() {
+
         let result = Electron.remote.dialog.showOpenDialogSync({
             properties: ["openDirectory"],
         });
         Scan.scan(result[0]);
         console.log(Scan.query(result[0]));
-        this.setState({currentPage:"mainviewpage"})
+        await this.setState({currentPage:"mainviewpage", rootPath: result[0], currentFolder: Scan.query(result[0]), folderTree: Scan.query(result[0])})
     }
     
+    renderTree = (nodes: dslib.File) => (
+        <TreeItem key={nodes.path} nodeId={nodes.path} label={path.basename(nodes.path ) + strConvert(nodes.size)}>
+            {Array.isArray(nodes.children) ? nodes.children.map((node) => this.renderTree(node)) : null}
+            {/* nodesFromChildren(nodes.children); */}
+        </TreeItem>
+    );
+
+    // nodesFromChildren (children?: [dslib.File]) {
+    //     let nodeChildren = [];
+
+    //     if (children) {
+    //         children.forEach(child => {
+    //             nodeChildren.push(this.renderTree(child)); 
+    //         });
+    //     }
+    //     return nodeChildren;   
+    // }
 
     render(): JSX.Element {
         if(this.state.currentPage == "scanpage") {
@@ -58,29 +90,42 @@ export class App extends Component<{}, { currentPage: string, currentFolder: dsl
                     </ThemeProvider>
                 </div>)
         }
-        else if(this.state.currentPage == "mainviewpage") {
-            return (
-            <ThemeProvider theme={createTheme({ palette: { type: "dark" } })}>
-                <SplitterLayout vertical={false}>
-                    {/* TreeView */}
-                    <div>
-                        Tom Put the TreeView Here
-                    </div>
-                    {/* Folder and Pie Views In Horizontal Splitter */}
-                    <div>
-                        <SplitterLayout vertical={true}>
-                            {/* FolderView */}
-                            <div>
-                                Alex Put the Folder View Here
-                            </div>
-                            {/* PieView */}
-                            <div>
-                                Ben Put the Pie Here
-                            </div>
-                        </SplitterLayout>
-                    </div>
-                </SplitterLayout>
-            </ThemeProvider>)
+        else if(this.state.currentPage == "mainviewpage") {   
+            if (this.state.folderTree.size != 0) {                                     
+                return <h1>
+                    <SplitterLayout vertical={false}>
+                        {/* TreeView */}
+                        <div>
+
+                        <TreeView
+                            defaultCollapseIcon={<ExpandMoreIcon />}
+                            defaultExpanded={['root']}
+                            defaultExpandIcon={<ChevronRightIcon />}
+                        >
+                            {this.renderTree(this.state.folderTree)}
+                        </TreeView>
+                
+                        </div>
+                        {/* Folder and Pie Views In Horizontal Splitter */}
+                        <div>
+                            <SplitterLayout vertical={true}>
+                                {/* FolderView */}
+                                <div>
+                                    Alex Put the Folder View Here
+                                </div>
+                                {/* PieView */}
+                                <div>
+                                    Ben Put the Pie Here
+                                </div>
+                            </SplitterLayout>
+                        </div>
+                    </SplitterLayout>
+                </h1>
+            }
+            else {
+                return null;
+            }
+
         }
     }
 }
