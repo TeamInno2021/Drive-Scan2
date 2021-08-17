11ccec0e6d356feac4200a207b989edbda5b4f48
Nigecat <nigel@nigelq.com>
2021-08-17 11:53:39 +1000

    Update build artifacts

--- dslib/src/unix/mod.rs
+++ dslib/src/unix/mod.rs
@@ -1,14 +1,14 @@
 use super::File;
 use std::path::{Path, PathBuf};
 
-pub fn verify(dir: &Path) -> Result<bool, Box<dyn ::std::error::Error>> {
+pub fn verify(_dir: &Path) -> Result<bool, Box<dyn ::std::error::Error>> {
     Ok(false)
 }
 
-pub fn scan(dir: PathBuf) -> Result<(), Box<dyn ::std::error::Error>> {
-    todo!();
+pub fn scan(_dir: PathBuf) -> Result<(), Box<dyn ::std::error::Error>> {
+    unimplemented!();
 }
 
-pub fn query(dir: PathBuf) -> Result<Option<File>, Box<dyn ::std::error::Error>> {
-    todo!();
+pub fn query(_dir: PathBuf) -> Result<Option<File>, Box<dyn ::std::error::Error>> {
+    unimplemented!();
 }
